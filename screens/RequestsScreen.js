ef2ab0ce2de7124b6e883ec664850a1eb6559756
karim-belhadjali <karim.belhadjali1@esprit.tn>
2022-10-31 00:36:57 +0100

    driver finished

--- screens/RequestsScreen.js
+++ screens/RequestsScreen.js
@@ -240,139 +240,145 @@ const RequestsScreen = () => {
   };
 
   return (
-    <View
-      style={tw`h-screen w-screen px-5 flex items-center pt-[${StatusBar.currentHeight}]`}
-    >
-      {!accepted && (
-        <View
-          style={tw`w-full flex flex-row justify-between items-center  my-5`}
-        >
-          <TouchableOpacity
-            onPress={() => navigation.navigate("MainDrawer")}
-            style={[
-              tw`bg-gray-50 p-3 mt-1 w-12 h-12 rounded-full  mr-3`,
-              { elevation: 50 },
-            ]}
-          >
-            <Entypo name="menu" size={25} color="#171717" />
-          </TouchableOpacity>
-
-          <ToggleSwitch
-            isOn={online}
-            onColor="#66CFC7"
-            offColor="#979797"
-            label="En ligne"
-            labelStyle={{
-              color: "black",
-              fontWeight: "900",
-              fontFamily: "Poppins-Regular",
-            }}
-            size="small"
-            onToggle={(isOn) => setonline(!online)}
-          />
-        </View>
-      )}
-      <View style={tw`w-full flex-row justify-start items-center`}>
-        <View
-          style={tw`rounded-full bg-[#979797] w-15 h-15 flex justify-center items-center`}
-        >
-          <AntDesign name="user" size={30} color={"#ffff"} />
-        </View>
-        <View style={tw`w-full  justify-center items-start ml-5`}>
-          <Text style={{ fontFamily: "Poppins-SemiBold", color: "#979797" }}>
-            Aslema, {user?.fullName}!
-          </Text>
-          <Text style={{ fontFamily: "Poppins-SemiBold", color: "#979797" }}>
-            <Text style={{ color: "#000000" }}>Beem</Text> vous souhaite
-          </Text>
-        </View>
-      </View>
+    <>
       <View
-        key={"separator"}
-        style={tw`bg-[#000000] opacity-10 h-[.45] mt-5 w-90`}
-      />
-      <View style={tw`flex justify-center items-start w-full ml-3 mt-5`}>
-        <Text
-          style={{ fontFamily: "Poppins-Bold", color: "#000000", fontSize: 22 }}
-        >
-          Liste des demandes
-        </Text>
-      </View>
-      {requests.length > 0 && (
-        <View style={tw`h-full w-full mt-5 flex items-center`}>
-          <FlatList
-            data={requests}
-            ItemSeparatorComponent={() => (
-              <View
-                style={[
-                  tw``,
-                  {
-                    height: 20,
-                  },
-                ]}
-              />
-            )}
-            renderItem={(request) => {
-              return (
+        style={tw`h-screen w-screen px-5 flex items-center pt-[${StatusBar.currentHeight}]`}
+      >
+        {!accepted && (
+          <View
+            style={tw`w-full flex flex-row justify-between items-center  my-5`}
+          >
+            <TouchableOpacity
+              onPress={() => navigation.navigate("MainDrawer")}
+              style={[
+                tw`bg-gray-50 p-3 mt-1 w-12 h-12 rounded-full  mr-3`,
+                { elevation: 50 },
+              ]}
+            >
+              <Entypo name="menu" size={25} color="#171717" />
+            </TouchableOpacity>
+
+            <ToggleSwitch
+              isOn={online}
+              onColor="#66CFC7"
+              offColor="#979797"
+              label="En ligne"
+              labelStyle={{
+                color: "black",
+                fontWeight: "900",
+                fontFamily: "Poppins-Regular",
+              }}
+              size="small"
+              onToggle={(isOn) => setonline(!online)}
+            />
+          </View>
+        )}
+        <View style={tw`w-full flex-row justify-start items-center`}>
+          <View
+            style={tw`rounded-full bg-[#979797] w-15 h-15 flex justify-center items-center`}
+          >
+            <AntDesign name="user" size={30} color={"#ffff"} />
+          </View>
+          <View style={tw`w-full  justify-center items-start ml-5`}>
+            <Text style={{ fontFamily: "Poppins-SemiBold", color: "#979797" }}>
+              Aslema, {user?.fullName}!
+            </Text>
+            <Text style={{ fontFamily: "Poppins-SemiBold", color: "#979797" }}>
+              <Text style={{ color: "#000000" }}>Beem</Text> vous souhaite
+            </Text>
+          </View>
+        </View>
+        <View
+          key={"separator"}
+          style={tw`bg-[#000000] opacity-10 h-[.45] mt-5 w-90`}
+        />
+        <View style={tw`flex justify-center items-start w-full ml-3 mt-5`}>
+          <Text
+            style={{
+              fontFamily: "Poppins-Bold",
+              color: "#000000",
+              fontSize: 22,
+            }}
+          >
+            Liste des demandes
+          </Text>
+        </View>
+        {requests.length > 0 && (
+          <View style={tw`h-full w-full mt-5 flex items-center`}>
+            <FlatList
+              data={requests}
+              ItemSeparatorComponent={() => (
                 <View
-                  style={tw`border-[#979797] bg-[#F5F5F5] border-[.35] h-40 rounded-md w-80 px-4`}
-                >
-                  <Text
-                    style={[
-                      tw`ml-2 mt-3`,
-                      {
-                        fontFamily: "Poppins-SemiBold",
-                        fontSize: 15,
-                        color: "#000000",
-                        opacity: 0.7,
-                      },
-                    ]}
+                  style={[
+                    tw``,
+                    {
+                      height: 20,
+                    },
+                  ]}
+                />
+              )}
+              renderItem={(request) => {
+                return (
+                  <View
+                    style={tw`border-[#979797] bg-[#F5F5F5] border-[.35] h-40 rounded-md w-80 px-4`}
                   >
-                    Le client vous attend à :
-                  </Text>
-                  <View style={tw`bg-[#000000] opacity-10 h-[.45] mt-1 `} />
-                  <View style={tw`w-full flex flex-row mt-5`}>
-                    <BlueDotSvg style={tw`mt-1 mr-2 pr-5`} />
                     <Text
                       style={[
-                        tw``,
-                        { fontFamily: "Poppins-SemiBold", fontSize: 15 },
+                        tw`ml-2 mt-3`,
+                        {
+                          fontFamily: "Poppins-SemiBold",
+                          fontSize: 15,
+                          color: "#000000",
+                          opacity: 0.7,
+                        },
                       ]}
-                      numberOfLines={1}
                     >
-                      {request.item.origin.description}
+                      Le client vous attend à :
                     </Text>
-                  </View>
-                  <View
-                    style={tw`flex flex-row justify-between items-center mt-3 pl-1 pr-2`}
-                  >
-                    <TouchableOpacity
-                      style={tw`rounded-full bg-[#F5F5F5] h-[11] w-[30] border-[#F74C00] border-[.35] p-2 flex flex-row justify-center items-center`}
-                      onPress={() => handleAnnuler(request.item)}
+                    <View style={tw`bg-[#000000] opacity-10 h-[.45] mt-1 `} />
+                    <View style={tw`w-full flex flex-row mt-5`}>
+                      <BlueDotSvg style={tw`mt-1 mr-2 pr-5`} />
+                      <Text
+                        style={[
+                          tw``,
+                          { fontFamily: "Poppins-SemiBold", fontSize: 15 },
+                        ]}
+                        numberOfLines={1}
+                      >
+                        {request.item.origin.description}
+                      </Text>
+                    </View>
+                    <View
+                      style={tw`flex flex-row justify-between items-center mt-3 pl-1 pr-2`}
                     >
-                      <Text style={styles.btnAnnuler}>Annuler</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                      style={tw`rounded-full bg-[#431879] h-[11] w-[35]  p-2 flex flex-row justify-center items-center`}
-                      onPress={() => handleAccept(request.item)}
-                    >
-                      <Text style={styles.btnAccepter}>Accepter</Text>
-                    </TouchableOpacity>
+                      <TouchableOpacity
+                        style={tw`rounded-full bg-[#F5F5F5] h-[11] w-[30] border-[#F74C00] border-[.35] p-2 flex flex-row justify-center items-center`}
+                        onPress={() => handleAnnuler(request.item)}
+                      >
+                        <Text style={styles.btnAnnuler}>Annuler</Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity
+                        style={tw`rounded-full bg-[#431879] h-[11] w-[35]  p-2 flex flex-row justify-center items-center`}
+                        onPress={() => handleAccept(request.item)}
+                      >
+                        <Text style={styles.btnAccepter}>Accepter</Text>
+                      </TouchableOpacity>
+                    </View>
                   </View>
-                </View>
-              );
-            }}
-            keyExtractor={(request) => request?.user?.phone}
-          />
-        </View>
-      )}
-      {requests.length < 1 && (
-        <View style={tw`w-full h-[80%] justify-center items-center`}>
-          <Text style={{ fontFamily: "Poppins-Regular", fontSize: 15 }}>
-            Pas de demande disponible pour le moment
-          </Text>
-        </View>
-      )}
+                );
+              }}
+              keyExtractor={(request) => request?.user?.phone}
+            />
+          </View>
+        )}
+        {requests.length < 1 && (
+          <View style={tw`w-full h-[80%] justify-center items-center`}>
+            <Text style={{ fontFamily: "Poppins-Regular", fontSize: 15 }}>
+              Pas de demande disponible pour le moment
+            </Text>
+          </View>
+        )}
+      </View>
       {accepted && (
         <View
           style={[
@@ -388,7 +394,7 @@ const RequestsScreen = () => {
           <ActivityIndicator size={80} color="#F74C00" />
         </View>
       )}
-    </View>
+    </>
   );
 };
 
